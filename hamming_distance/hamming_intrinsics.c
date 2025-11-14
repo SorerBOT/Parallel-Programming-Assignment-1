90519d0dfbc71e410095e38ae88b4e61d6605f69
SorerBOT <alonfiller@gmaill.com>
2025-11-14 18:53:41 +0200

    (Feat) hamming distance

--- hamming_distance/hamming_intrinsics.c
+++ hamming_distance/hamming_intrinsics.c
@@ -3,6 +3,8 @@
 #include <string.h>
 
 #define MAX_STR 256
+#define CHUNK_SIZE 16
+#define MATCH_VALUE 0xFF
 
 /*
  * Algorithm:
@@ -20,10 +22,9 @@
  * and then dividing by 0xFF will get us the result.
  */
 
-size_t strln(const char* str)
+size_t strlen(const char* str)
 {
     const char* init_address = str;
-    int len = 0;
     while (*str != '\0')
     {
         ++str;
@@ -48,34 +49,55 @@ int min(int x, int y)
     return x > y ? y : x;
 }
 
+int hamming_dist_chunk(const char* str_ptr1, const char* str_ptr2)
+{
+    const __m128i str_xmm1         = _mm_loadu_si128((const __m128i_u *) str_ptr1);
+    const __m128i str_xmm2         = _mm_loadu_si128((const __m128i_u *) str_ptr2);
+    const __m128i empty_buffer_xmm = _mm_setzero_si128();
+
+    const __m128i res              = _mm_cmpeq_epi8(str_xmm1, str_xmm2);
+    const __m128i sum_matches      = _mm_sad_epu8(res, empty_buffer_xmm);
+    const size_t  sum_lower        = _mm_extract_epi16(sum_matches, 0x0);
+    const size_t  sum_upper        = _mm_extract_epi16(sum_matches, 0x4);
+    return CHUNK_SIZE - (sum_lower + sum_upper) / MATCH_VALUE;
+}
+
 int hamming_dist(char str1[MAX_STR], char str2[MAX_STR])
 {
     const size_t len1           = strlen(str1);
     const size_t len2           = strlen(str2);
     const size_t len_min        = min(len1, len2);
 
-    const size_t dist           = abs((int)len1 - (int)len2);
-    const size_t iters          = len_min / 16 + (len_min % 16 < 0); // in case there's a remainder
+    size_t dist                 = abs((int)len1 - (int)len2);
+    const size_t iters          = len_min / CHUNK_SIZE; // in case there's a remainder
 
-    const char* str_ptr1        = str1;
-    const char* str_ptr2        = str2;
+    char* str_ptr1              = str1;
+    char* str_ptr2              = str2;
 
     for (size_t i = 0;
             i < iters;
-            str_ptr1 += 16,
-            str_ptr2 += 16,
-            i += 1)
+            str_ptr1 += CHUNK_SIZE,
+            str_ptr2 += CHUNK_SIZE,
+            ++i)
     {
-        const __m128i str_xmm1         = _mm_loadu_si128((const __m128i_u *) str_ptr1);
-        const __m128i str_xmm2         = _mm_loadu_si128((const __m128i_u *) str_ptr2);
-        const __m128i empty_buffer_xmm = _mm_setzero_si128();
-
-        const __m128i res              = _mm_cmpeq_epi8(str_xmm1, str_xmm2);
-        const __m128i sum              = _mm_sad_epu8(res, empty_buffer_xmm);
-        const size_t  sum_lower        = _mm_extract_epi16(sum, 0x0);
-        const size_t  sum_upper        = _mm_extract_epi16(sum, 0x4);
-
-
-
+        dist += hamming_dist_chunk(str_ptr1, str_ptr2);
     }
+
+    if (len_min % CHUNK_SIZE > 0)
+    {
+        /*
+         * Removing garbage from strings
+         */
+        const size_t current_len1     = strlen(str_ptr1);
+        const size_t current_len2     = strlen(str_ptr2);
+        const size_t current_len_min  = min(current_len1, current_len2);
+
+        char buffer_tmp1[CHUNK_SIZE] = { 0 };
+        char buffer_tmp2[CHUNK_SIZE] = { 0 };
+        memcpy(buffer_tmp1, str_ptr1, current_len_min);
+        memcpy(buffer_tmp2, str_ptr2, current_len_min);
+        dist += hamming_dist_chunk(buffer_tmp1, buffer_tmp2);
+    }
+
+    return dist;
 }
